4c2f09cd25f1d627b8556cdddf95fd6676647b7b
unknown <jjatuah@gmail.com>
2021-11-29 21:55:39 +0100

    Second commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App"
+import {BrowserRouter as Router} from "react-router-dom"
+
+
+ReactDOM.render(<div>
+<Router>
+<App></App>
+</Router>
+</div>, document.getElementById("root"));
+
+
+
